fa5673d31be490dcdd7c44d5ee862bc1726556fa
nicklefritz <nrcoover@gmail.com>
2026-09-03 06:29:55 -0400

    Updating navigation with safe velocity and avoidance
    Utilizes time_horizon values and ups max speed for recalculating safe velocity.

--- nav_demo/mine.gd
+++ nav_demo/mine.gd
@@ -5,9 +5,15 @@ extends Node2D
 @export var range_px: float = 100.0
 
 
+@onready var nav_obstacle: NavigationObstacle2D = $NavObstacle
+
+
 var min_y: float
 var max_y: float
 var dir: int = 1
+
+
+var _velocity: Vector2
 
 
 func _ready() -> void:
@@ -21,4 +27,6 @@ func _physics_process(delta: float) -> void:
 		dir = -1
 	elif global_position.y < min_y:
 		dir = 1
-	global_position += speed * Vector2(0, dir) * delta
+		_velocity = speed * Vector2(0, dir)
+		nav_obstacle.velocity = _velocity
+	global_position += _velocity * delta
--- nav_demo/ship.gd
+++ nav_demo/ship.gd
@@ -8,20 +8,26 @@ extends Node2D
 @onready var nav_agent_2d: NavigationAgent2D = $NavAgent2D
 
 
+var _last_delta: float = 0.0
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("set_target"):
 		nav_agent_2d.target_position = get_global_mouse_position()
 
 
 func _physics_process(delta: float) -> void:	
+	
+	_last_delta = delta
 	update_label()
 	
 	if nav_agent_2d.is_navigation_finished(): return
 	
 	var next_path_position: Vector2 = nav_agent_2d.get_next_path_position()
-	var direction: Vector2 = global_position.direction_to(next_path_position)
-	position += direction * speed * delta
-	rotation = direction.angle()
+	var new_velocity: Vector2 = global_position.direction_to(next_path_position) * speed
+	#position += direction * speed * delta
+	#rotation = direction.angle()
+	nav_agent_2d.velocity = new_velocity
 
 
 func update_label() -> void:
@@ -30,3 +36,12 @@ func update_label() -> void:
 	debug_text += "Reached: %s\n" % nav_agent_2d.is_target_reached()
 	debug_text += "Finished: %s" % nav_agent_2d.is_navigation_finished()
 	label.text = debug_text
+
+
+func _on_nav_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	global_position += safe_velocity * _last_delta
+	#rotation = safe_velocity.angle()
+	
+	if safe_velocity.length() > 0.01:
+		# this will smooth our object's rotation during velocity recalculations
+		rotation = rotate_toward(rotation, safe_velocity.angle(), deg_to_rad(360) * _last_delta)
