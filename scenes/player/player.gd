39402d3dae33a1645c7de89b6828116089e9547c
nicklefritz <nrcoover@gmail.com>
2026-09-09 00:06:46 -0400

    Add lighting

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -13,5 +13,8 @@ func _physics_process(delta: float) -> void:
 func handle_input() -> void:
 	var input: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input.normalized() * speed
-	rotation = velocity.angle()
+	
+	if !is_zero_approx(velocity.length()):
+		rotation = velocity.angle()
+	
 	move_and_slide()
